be420b00b2162e585210bd2e82d27f572c1a80be
Robert Laukhuff <robert@laukcoder.com>
2019-09-19 18:57:04 -0400

    Complete Guessing Game Class

--- lib/guessing_game.rb
+++ lib/guessing_game.rb
@@ -1,3 +1,35 @@
 class GuessingGame
+  def initialize(min, max)
+    @min = min
+    @max = max
+    @secret_num = rand(max)
+    @num_attempts = 0
+    @game_over = false
+  end
 
+  def num_attempts
+    @num_attempts
+  end
+
+  def game_over?
+    @game_over
+  end
+
+  def check_num(number)
+    @num_attempts += 1
+    if number == @secret_num
+      @game_over = true
+      print "you win"
+    elsif number > @secret_num
+      print "too big"
+    else number < @secret_num
+      print "too small"
+    end
+  end
+
+  def ask_user
+    print "enter a number"
+    guess = gets.chomp.to_i
+    check_num(guess)
+  end
 end
